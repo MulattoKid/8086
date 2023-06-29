b423022cfb3559a8058af239412478295ebbb48e
Daniel Fedai Larsen <danielfedailarsen@hotmail.com>
2023-06-29 19:47:23 +0200

    Run through all test files each run

--- main.c
+++ main.c
@@ -28,74 +28,82 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <stdlib.h>
 #include <string.h>
 
+#define ENCODED_ASSEMBLY_FILE_COUNT 4
+
+static const char* encoded_assembly_files[ENCODED_ASSEMBLY_FILE_COUNT] = {
+    "./test_files/listing_0037_single_register_mov",
+    "./test_files/listing_0038_many_register_mov",
+    "./test_files/listing_0039_more_movs",
+    "./test_files/listing_0039_more_movs",
+};
+
 int main(int argc, char** argv)
 {
-    /* Get file to process */
-    if (argc != 2)
+    /* Decode all files */
+    for (uint8_t i = 0; i < ENCODED_ASSEMBLY_FILE_COUNT; i++)
     {
-        printf("[ARGS] Invalid number of arguments (%i)\n", argc);
-        return -1;
+        /* Print current file */
+        printf("Decoding '%s'\n", encoded_assembly_files[i]);
+
+        /* Read file */
+        FILE* file = fopen(encoded_assembly_files[i], "rb");
+        if (file == NULL)
+        {
+            printf("\t[FILE] Failed to open file '%s'\n", encoded_assembly_files[i]);
+            return -1;
+        }
+        fseek(file, 0, SEEK_END);
+        uint32_t file_size_original = (uint32_t)ftell(file);
+        fseek(file, 0, SEEK_SET);
+        uint8_t* file_data_original = malloc(file_size_original);
+        fread(file_data_original, file_size_original, 1, file);
+        fclose(file);
+
+        /* Open file to write decoded stream into */
+        file = fopen("tmp.asm", "w");
+        if (file == NULL)
+        {
+            printf("\t[FILE] Failed to open file 'tmp.txt'\n");
+            return -1;
+        }
+
+        /* Decode instructions */
+        decoder_decode_stream(file_data_original, file_size_original, file);
+
+        /* Close file */
+        fflush(file);
+        fclose(file);
+
+        /* Encode output assembly file */
+        system("nasm tmp.asm");
+
+        /* Read in encoded data */
+        file = fopen("tmp", "rb");
+        if (file == NULL)
+        {
+            printf("\t[FILE] Failed to open file 'tmp'\n");
+            return -1;
+        }
+        fseek(file, 0, SEEK_END);
+        uint32_t file_size_result = (uint32_t)ftell(file);
+        fseek(file, 0, SEEK_SET);
+        uint8_t* file_data_result = malloc(file_size_result);
+        fread(file_data_result, file_size_result, 1, file);
+        fclose(file);
+
+        /* Compare original and new result */
+        if (file_size_original != file_size_result)
+        {
+            printf("\t[COMPARE] Original size (%u) and result size (%u) aren't equal\n", file_size_original, file_size_result);
+            return;
+        }
+        if (memcmp(file_data_original, file_data_result, file_size_result) != 0)
+        {
+            printf("\t[COMPARE] Content of original file and result file aren't the same\n");
+            return;
+        }
+        printf("\t[COMPARE] Original and result file are equal\n\n");
     }
-    const char* file_name = argv[1];
-
-    /* Read file */
-    FILE* file = fopen(file_name, "rb");
-    if (file == NULL)
-    {
-        printf("[FILE] Failed to open file '%s'\n", file_name);
-        return -1;
-    }
-    fseek(file, 0, SEEK_END);
-    uint32_t file_size_original = (uint32_t)ftell(file);
-    fseek(file, 0, SEEK_SET);
-    uint8_t* file_data_original = malloc(file_size_original);
-    fread(file_data_original, file_size_original, 1, file);
-    fclose(file);
-
-    /* Open file to write decoded stream into */
-    file = fopen("tmp.asm", "w");
-    if (file == NULL)
-    {
-        printf("[FILE] Failed to open file 'tmp.txt'\n");
-        return -1;
-    }
-
-    /* Decode instructions */
-    decoder_decode_stream(file_data_original, file_size_original, file);
-
-    /* Close file */
-    fflush(file);
-    fclose(file);
-
-    /* Encode output assembly file */
-    system("nasm tmp.asm");
-
-    /* Read in encoded data */
-    file = fopen("tmp", "rb");
-    if (file == NULL)
-    {
-        printf("[FILE] Failed to open file 'tmp'\n");
-        return -1;
-    }
-    fseek(file, 0, SEEK_END);
-    uint32_t file_size_result = (uint32_t)ftell(file);
-    fseek(file, 0, SEEK_SET);
-    uint8_t* file_data_result = malloc(file_size_result);
-    fread(file_data_result, file_size_result, 1, file);
-    fclose(file);
-
-    /* Compare original and new result */
-    if (file_size_original != file_size_result)
-    {
-        printf("[COMPARE] Original size (%u) and result size (%u) aren't equal\n", file_size_original, file_size_result);
-        return;
-    }
-    if (memcmp(file_data_original, file_data_result, file_size_result) != 0)
-    {
-        printf("[COMPARE] Content of original file and result file aren't the same\n");
-        return;
-    }
-    printf("[COMPARE] Original and result file are equal\n");
 
     return 0;
 }
